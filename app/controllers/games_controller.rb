32a30795c5466b764f33ae13be5a02214ec73296
Lucia Fontana <lucia.fontana1987@gmail.com>
2022-11-21 16:11:26 +0100

    Game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    filepath = URI.open("https://wagon-dictionary.herokuapp.com/#{params['word']}")
+    info = JSON.parse(filepath.read)
+
+    if params['word'].chars.all? { |letter| attempt.count(letter) <= grid.count(letter) }
+      if info['found']
+        @answer = "Congratulations #{params['word']} is a valid English word"
+      else
+        @answer = "Sorry but #{params['word']} doesn't seem a valid word"
+      end
+    else
+      @answer = "Sorry but #{params['word']} cannot be built out of #{@letters}"
+    end
+  end
+end
